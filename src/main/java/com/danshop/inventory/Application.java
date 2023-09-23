4d151f80f5c3cc76759db6e0e84f3fcac206b373
Daniele Parisi <neb.inf@gmail.com>
2023-09-23 16:42:08 +0200

    Add dan-service-tech-starter dependency

--- src/main/java/com/danshop/inventory/Application.java
+++ src/main/java/com/danshop/inventory/Application.java
@@ -1,33 +1,16 @@
 package com.danshop.inventory;
 
-import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @Slf4j
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        try {
-            SpringApplication
-                    .run(Application.class, args);
-
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
-    }
-
-    @Bean
-    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(Environment environment) {
-        String serviceName = environment.getProperty("spring.application.name");
-
-        /* Exposing application name to Prometheus, so that it's shown on Grafana */
-        return registry -> registry.config().commonTags("application", serviceName);
+        SpringApplication
+                .run(Application.class, args);
     }
 
 }
